70b28264d579ed9530585df569278fb62dcd42b8
Tom Kistner <tom@kistner.nu>
2019-03-09 12:37:33 +0100

    Add debian bureaucracy

---
+++ build/launchpad-ppa.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+cd $(dirname $(realpath $0))/../src
+
+DATE=$(date -R)
+VERSION="1.0.1-1"
+
+for RELEASE in cosmic bionic
+do
+
+cat >debian/changelog <<END
+roccat-vulcan ($VERSION~$RELEASE) $RELEASE; urgency=medium
+
+  * Update to $VERSION
+
+ -- Tom Kistner <tom@kistner.nu>  $DATE
+END
+
+debuild -S | tee debuild.log 2>&1
+dput ppa:duncanthrax/roccat-vulcan "$(perl -ne 'print $1 if /dpkg-genchanges --build=source >(.*)/' debuild.log)"
+rm -f debuild.log
+
+done
