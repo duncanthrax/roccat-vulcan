c5eb8207582d8c8b7aae8cd547bf0e47ff971483
SpiritCroc <dev@spiritcroc.de>
2019-04-06 10:16:19 +0200

    rv_get_keycode: Add KEY_FN support

--- src/evdev.c
+++ src/evdev.c
@@ -127,6 +127,7 @@ int rv_init_evdev() {
 }
 
 int rv_get_keycode(char *ev_keyname) {
+	if (strncmp("KEY_FN", ev_keyname, 7) == 0) return 76;
 	int ev_code = libevdev_event_code_from_name(EV_KEY, ev_keyname);
 	if (ev_code < 0 || ev_code > 254) return -1;
 	return (rv_ev2rv[ev_code] == 0xff) ? -1 : rv_ev2rv[ev_code];
